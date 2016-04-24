97c3be318e69fefd9c1b9e773d3cf28a83d5e91c
realcow <realcow86@gmail.com>
2016-04-25 03:30:05 +0900

    parsing IMAGE_SECTION_HEADERs

--- WebPEAnalyzer/app.ts
+++ WebPEAnalyzer/app.ts
@@ -12,6 +12,22 @@ class ImageFileHeader {
     NumberOfSymbols: number;
     SizeOfOptionalHeader: number;
     Characteristics: number;
+    static kSize: number = 20;
+}
+
+class ImageSectionHeader {
+    Name: string;
+    PhysicalAddress: number;
+    VirtualSize: number;
+    VirtualAddress: number;
+    SizeOfRawData: number;
+    PointerToRawData: number;
+    PointerToRelocations: number;
+    PointerToLinenumbers: number;
+    NumberOfRelocations: number;
+    NumberOfLinenumbers: number;
+    Characteristics: number;
+    static kSize: number = 40;
 }
 
 // Parse functions
@@ -27,8 +43,30 @@ function parseImageFileHeader(dv: DataView, fileOffset: number): ImageFileHeader
     return ifh;
 }
 
-function parseImageDataDirectory(dv: DataView, fileOffset: number) : ImageDataDirectory {
+function parseImageDataDirectory(dv: DataView, fileOffset: number): ImageDataDirectory {
     return new ImageDataDirectory(dv.getUint16(fileOffset, true), dv.getUint16(fileOffset + 4, true));
+}
+
+function parseImageSectionHeader(dv: DataView, fileOffset: number): ImageSectionHeader {
+    var ish = new ImageSectionHeader;
+    ish.Name = "";
+    for (var i: number = 0; i < 8; i++) {
+        var c = dv.getUint8(fileOffset + i);
+        if (c == 0) {
+            break;
+        }
+        ish.Name += String.fromCharCode(c);
+    }
+    ish.VirtualSize = dv.getUint32(fileOffset + 0x08, true);
+    ish.VirtualAddress = dv.getUint32(fileOffset + 0x0C, true);
+    ish.SizeOfRawData = dv.getUint32(fileOffset + 0x10, true);
+    ish.PointerToRawData = dv.getUint32(fileOffset + 0x14, true);
+    ish.PointerToRelocations = dv.getUint32(fileOffset + 0x18, true);
+    ish.PointerToLinenumbers = dv.getUint32(fileOffset + 0x1C, true);
+    ish.NumberOfRelocations = dv.getUint32(fileOffset + 0x20, true);
+    ish.NumberOfLinenumbers = dv.getUint32(fileOffset + 0x22, true);
+    ish.Characteristics = dv.getUint32(fileOffset + 0x22, true);
+    return ish;
 }
 
 function analyze(data: ArrayBuffer) {
@@ -42,6 +80,17 @@ function analyze(data: ArrayBuffer) {
     var ntHeaderOffset : number = dataView.getUint32(0x3C, true); // lf_anew
 
     var ifh: ImageFileHeader = parseImageFileHeader(dataView, ntHeaderOffset + 4);
+
+    // parse section headers
+    console.log("=== Sections ===");
+    var sectionHeaderBaseOffset = ntHeaderOffset + 4 + ImageFileHeader.kSize + ifh.SizeOfOptionalHeader;
+    var sectionHeaders: ImageSectionHeader[] = new Array<ImageSectionHeader>();
+    for (var i: number = 0; i < ifh.NumberOfSections; i++) {
+        var ish = parseImageSectionHeader(dataView, sectionHeaderBaseOffset + i * ImageSectionHeader.kSize);
+        sectionHeaders.push(ish);
+        console.log(ish.Name + ", VA: " + ish.VirtualAddress + ", VirtualSize: " + ish.VirtualSize + ", Offset: " + ish.PointerToRawData);
+    }
+
     var importDir: ImageDataDirectory = parseImageDataDirectory(dataView, ntHeaderOffset + 104);
 }
 
