d767a254df751fc5b6f0b778cb146b0cc02c804e
realcow <realcow86@gmail.com>
2016-04-25 00:36:24 +0900

    parse IMAGE_FILE_HEADER

--- WebPEAnalyzer/app.ts
+++ WebPEAnalyzer/app.ts
@@ -1,10 +1,48 @@
-﻿function analyze(data: ArrayBuffer) {
+﻿// PE structure definitions
+class ImageDataDirectory {
+    constructor(public rva: number, public size: number)
+    { }
+}
+
+class ImageFileHeader {
+    Machine: number;
+    NumberOfSections: number;
+    TimeDateStamp: number;
+    PointerToSymbolTable: number;
+    NumberOfSymbols: number;
+    SizeOfOptionalHeader: number;
+    Characteristics: number;
+}
+
+// Parse functions
+function parseImageFileHeader(dv: DataView, fileOffset: number): ImageFileHeader {
+    var ifh = new ImageFileHeader;
+    ifh.Machine = dv.getUint16(fileOffset + 0x00, true);
+    ifh.NumberOfSections = dv.getUint16(fileOffset + 0x02, true);
+    ifh.TimeDateStamp = dv.getUint16(fileOffset + 0x04, true);
+    ifh.PointerToSymbolTable = dv.getUint32(fileOffset + 0x08, true);
+    ifh.NumberOfSymbols = dv.getUint32(fileOffset + 0x0C, true);
+    ifh.SizeOfOptionalHeader = dv.getUint16(fileOffset + 0x10, true);
+    ifh.Characteristics = dv.getUint16(fileOffset + 0x12, true);
+    return ifh;
+}
+
+function parseImageDataDirectory(dv: DataView, fileOffset: number) : ImageDataDirectory {
+    return new ImageDataDirectory(dv.getUint16(fileOffset, true), dv.getUint16(fileOffset + 4, true));
+}
+
+function analyze(data: ArrayBuffer) {
     var byteView: Uint8Array = new Uint8Array(data);
-    if (String.fromCharCode(byteView[0]) == 'M' && String.fromCharCode(byteView[1]) == 'Z') {
-        console.log("it is likely PE file");
-    } else {
-        console.log("it is NOT likely PE file");
+    var dataView: DataView = new DataView(data);
+    if (dataView.getUint16(0, true) != 0x5A4D /*MZ*/) {
+        console.log("Signature mismatched");
+        return;
     }
+
+    var ntHeaderOffset : number = dataView.getUint32(0x3C, true); // lf_anew
+
+    var ifh: ImageFileHeader = parseImageFileHeader(dataView, ntHeaderOffset + 4);
+    var importDir: ImageDataDirectory = parseImageDataDirectory(dataView, ntHeaderOffset + 104);
 }
 
 function onFileChange(event: Event) {
